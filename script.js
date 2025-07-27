7004ea2f0900bccc9a7027e95f56b1b1b2a7ab5a
Rajat Pathania <83996978+pathania0001@users.noreply.github.com>
2025-07-27 14:20:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,78 @@
 function myRace(promises) {
-  // implementation
+    return new Promise((resolve,reject)=>{
+	  let isResolved = false;
+	  promises.forEach((promise)=>{
+		  Promise.resolve(promise).then((data) =>{
+				  if(!isResolved){
+					  isResolved = true;
+					  resolve(data);
+				  }}).catch((error)=>{
+				  if(!isResolved){
+					  isResolved = true;
+					  reject(error);
+				  }})
+	  })
+  })
 }
-
 function myAny(promises) {
-  // implementation
+  return new Promise((resolve,reject)=>{
+    if(promises.length === 0)
+		reject(promises);
+	  let count = 0;
+	  const isAllRejected = Array(promises.length);
+
+	  promises.forEach((promise,index)=>{
+		  Promise.resolve(promise)
+			  .then( data => resolve(data))
+			  .catch((error)=>{
+			  isAllRejected[index] = error;
+				 count++;
+				  if(count === promises.length)
+					  reject(new AggregateError(rejectedErrors, "All promises were rejected"));
+		  })
+	  })
+  })
 }
 
 function myAll(promises) {
-  // implementation
+  return new Promise ((resolve,reject)=>{
+	  if(promises.length===0)
+	   resolve(promises);
+	  let isAllResolved = Array(promises.length);
+	  let count = 0;
+	  promises.forEach((promise,idx)=>{
+		  Promise.resolve(promise).then((data)=>{
+			  isAllResolved[idx] = data;
+			  count++;
+			  if(count === promises.length)
+				  resolve(isAllResolved)
+		  }).catch((error)=>{
+			  reject(error);
+		  })
+	  })
+  })
 }
 
 function myAllSettled(promises) {
-  // implementation
+  return new Promise((resolve,reject)=>{
+	  if(promises.length===0)
+	   resolve(promises);
+      const isAllResolved = Array(promises.length);
+	  let count = 0;
+	  promises.forEach((promise,index)=>{
+		  Promise.resolve(promise).then((data)=>{
+			  count++;
+			  isAllResolved[index] = {status:"fullfilled",value:data};
+			  if(count === promises.length)
+				  resolve(isAllResolved);
+		  }).catch((error)=>{
+			  count++;
+			  isAllResolved[index] = {status:"rejected",value:error};
+			  if(count === promises.length)
+				  resolve(isAllResolved);
+		  })
+	  })
+  })
 }
 
 module.exports = {
